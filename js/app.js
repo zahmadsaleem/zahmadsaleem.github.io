f8b9b510e166a9951d391a659b07e8bd4d7655ad
Ahmad Saleem Z <azsaleem11@gmail.com>
2019-10-05 11:11:49 +0530

    navbar edit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     "use strict";
     var myapp = new Vue({
         el: '#app',
@@ -10,13 +10,9 @@
             imgurl: ''
         },
         methods: {
-            showMsgBox: function(imgurl) {
+            showMsgBox: function (imgurl) {
                 this.imgurl = imgurl
                 this.$refs['mm-modal'].show()
-            },
-            changeCover: function(ref,src,event) {
-                var cover = this.$refs[ref + '-cover'][0].$el;
-                cover.src = 'img/bg-img/' +src;
             }
         }
 
