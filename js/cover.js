b33edb38b3355cb191bf159723e82538bb7788f5
ahmadsaleemz <51987839+ahmadsaleemz@users.noreply.github.com>
2020-02-02 13:31:34 +0530

    fixed p5 lag

--- js/cover.js
+++ js/cover.js
@@ -1,96 +1,102 @@
-let wwidth;
-let hheight;
-let divX;
-let divY;
-let img;
-var cnv;
-var ratio;
+async function createp5sketch(p) {
+    let wwidth;
+    let hheight;
+    let divX;
+    let divY;
+    let img;
+    var cnv;
+    var ratio;
 
-function setup() {
-    ratio = windowWidth / windowHeight;
-    cnv = createCanvas();
-    setCanvasSize();
-    cnv.parent('cover-main');
-    centerCanvas();
-    noCursor();
-    img = loadImage('img/cover-bg.jpg');
-}
+    img = await p.loadImage('img/cover-bg.jpg');
 
-function centerCanvas() {
-    var x = (windowWidth - width) / 2 ;
-    var y = (windowHeight - height) / 2 + 35;
-    cnv.position(x, y);
-}
-
-function setCanvasSize() {
-    if (ratio < 1.4 && ratio > 0.6) { //if height almost equal to width
-        // console.log("almost square",ratio);
-        let smaller = windowWidth > windowHeight ? windowHeight - 50 : windowWidth -50;
-        wwidth = smaller;
-        hheight = smaller;
-        divX = 6;
-        divY = 3;
-    } else if (ratio > 1.2) { //if width more than height
-        // console.log("landscape",ratio);
-        wwidth = windowWidth - 50 ;
-        hheight = wwidth / 2;
-        divX = 12;
-        divY = 3;
-    } else { //if height more than width
-        // console.log("portrait",ratio);
-        hheight = windowHeight - 50;
-        wwidth = hheight / 2;
-        divX = 6;
-        divY = 6;
-    }
-    resizeCanvas(wwidth, hheight);
-}
-
-function draw() {
-    spanX = wwidth / divX;
-    spanY = hheight / divY;
-    var bgcol = color('rgba(255,255,255,0)');
-    image(img, 0, 0, wwidth, hheight);
-    if (ratio > 1.2) {
-        image(img, wwidth, 0, wwidth, hheight);
-    } else if (ratio < 0.8) {
-        image(img, 0, hheight, wwidth, hheight);
-    }
-    background(bgcol);
-
-
-    if (!mouseIsPressed) {
-        noStroke();
-        fill(color('rgba(170,170,170,.4)'));
-        ellipse(mouseX, mouseY, 100, 100);
-        fill(color('rgba(190,190,190,.2)'));
-        ellipse(mouseX, mouseY, 90, 90);
+    p.setup = function() {
+        ratio = p.windowWidth / p.windowHeight;
+        cnv = p.createCanvas();
+        setCanvasSize();
+        cnv.parent('cover-main');
+        centerCanvas();
+        p.noCursor();
     }
 
-    stroke('#fff');
-    strokeWeight(5);
+    function centerCanvas() {
+        var x = (p.windowWidth - p.width) / 2;
+        var y = (p.windowHeight - p.height) / 2 + 35;
+        cnv.position(x, y);
+    }
 
-    for (let i = 0; i < divX; i++) {
-        for (let j = 0; j < divY; j++) {
-            let col = 220;
-            col -= dist(mouseX, mouseY, i * spanX, j * spanY) / 3;
-            var colHere = color(col + 10, col + 15, col);
-            colHere.setAlpha(90);
-            fill(colHere);
-            rect(i * spanX, j * spanY, spanX, spanY);
+    function setCanvasSize() {
+        if (ratio < 1.4 && ratio > 0.6) { //if height almost equal to width
+            // console.log("almost square",ratio);
+            let smaller = p.windowWidth > p.windowHeight ? p.windowHeight - 50 : p.windowWidth - 50;
+            wwidth = smaller;
+            hheight = smaller;
+            divX = 6;
+            divY = 3;
+        } else if (ratio > 1.2) { //if width more than height
+            // console.log("landscape",ratio);
+            wwidth = p.windowWidth - 50;
+            hheight = wwidth / 2;
+            divX = 12;
+            divY = 3;
+        } else { //if height more than width
+            // console.log("portrait",ratio);
+            hheight = p.windowHeight - 50;
+            wwidth = hheight / 2;
+            divX = 6;
+            divY = 6;
+        }
+        p.resizeCanvas(wwidth, hheight);
+    }
 
+    p.draw = function() {
+        spanX = wwidth / divX;
+        spanY = hheight / divY;
+        var bgcol = p.color('rgba(255,255,255,0)');
+        p.image(img, 0, 0, wwidth, hheight);
+        if (ratio > 1.2) {
+            p.image(img, wwidth, 0, wwidth, hheight);
+        } else if (ratio < 0.8) {
+            p.image(img, 0, hheight, wwidth, hheight);
+        }
+        p.background(bgcol);
+
+
+        if (!p.mouseIsPressed) {
+            p.noStroke();
+            p.fill(p.color('rgba(170,170,170,.4)'));
+            p.ellipse(p.mouseX, p.mouseY, 100, 100);
+            p.fill(p.color('rgba(190,190,190,.2)'));
+            p.ellipse(p.mouseX, p.mouseY, 90, 90);
+        }
+
+        p.stroke('#fff');
+        p.strokeWeight(5);
+
+        for (let i = 0; i < divX; i++) {
+            for (let j = 0; j < divY; j++) {
+                let col = 220;
+                col -= p.dist(p.mouseX, p.mouseY, i * spanX, j * spanY) / 3;
+                var colHere = p.color(col + 10, col + 15, col);
+                colHere.setAlpha(90);
+                p.fill(colHere);
+                p.rect(i * spanX, j * spanY, spanX, spanY);
+
+            }
+        }
+        if (p.mouseIsPressed) {
+            p.fill(p.color('rgba(255,235,56,0.3)'));
+            p.rect(p.int(p.mouseX / spanX) * spanX, p.int(p.mouseY / spanY) * spanY, spanX, spanY);
+            p.noStroke();
+            p.rect(p.int(p.mouseX / spanX) * spanX - 5, int(p.mouseY / spanY) * spanY - 5, spanX + 10, spanY + 10);
         }
     }
-    if (mouseIsPressed) {
-        fill(color('rgba(255,235,56,0.3)'));
-        rect(int(mouseX / spanX) * spanX, int(mouseY / spanY) * spanY, spanX, spanY);
-        noStroke();
-        rect(int(mouseX / spanX) * spanX - 5, int(mouseY / spanY) * spanY - 5, spanX + 10, spanY + 10);
+
+    p.windowResized = function() {
+        ratio = p.windowWidth / p.windowHeight;
+        setCanvasSize();
+        centerCanvas();
     }
 }
 
-function windowResized() {
-    ratio = windowWidth / windowHeight;
-    setCanvasSize();
-    centerCanvas();
-}
+
+let myp5 = new p5(createp5sketch);
