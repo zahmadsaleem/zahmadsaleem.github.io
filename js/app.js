bb8fd7fab8b5e6c92b529c795669859396f43621
ahmadsaleemz <51987839+ahmadsaleemz@users.noreply.github.com>
2020-02-02 20:42:56 +0530

    multi-view beta 0.2

--- js/app.js
+++ js/app.js
@@ -102,6 +102,26 @@
         }
     }
 
+    const contactView = {
+        template: `
+            <div id="contact" class="m-0 p-5 bg-dark text-light text-center">
+            <div class="w-100 mx-auto">
+                <h3 class="font-heavy py-2">AHMAD SALEEM Z</h3>
+                <b-card-text>
+                    <b-link href="mailto:azsaleem11@gmail.com">azsaleem11@gmail.com</b-link>
+                    <br>+91 9944405317<br>
+                    <b-link href="http://linkedin.com/in/ahmadsaleemz" class="text-secondary">LinkedIn </b-link>
+                    <span class="text-secondary">-</span>
+                    <b-link href="http://instagram.com/ah.mad.so.lame" class="text-secondary">Instagram</b-link>
+                    <span class="text-secondary">-</span>
+                    <b-link href="http://www.facebook.com/ahmadsaleemz" class="text-secondary">Facebook</b-link>
+                    <span class="text-secondary">-</span>
+                    <b-link href="http://www.github.com/zahmadsaleem" class="text-secondary">GitHub</b-link>
+                </b-card-text>
+            </div>
+            </div>`
+    }
+
     const router = new VueRouter({
         mode: 'history',
         routes: [
@@ -121,7 +141,10 @@
                 path: '/resume',
                 component: resumeView
             },
-
+            {
+                path: '/contact',
+                component: contactView
+            },
 
         ]
     });
