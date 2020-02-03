6f709632483a9086a7b51fe5eb8d1a496c347913
ahmadsaleemz <51987839+ahmadsaleemz@users.noreply.github.com>
2020-02-03 10:01:47 +0530

    multi-view beta 1.3

--- js/app.js
+++ js/app.js
@@ -64,7 +64,7 @@
         <div v-p5viewed id="cover-main" class="text-center cover-container d-flex mx-auto flex-column">
         <main role="main" class="inner cover my-auto">
             <div class="justified-content-center mb-3">
-                <p class="h1 d-inline px-5 py-1 bg-light">Portfolio</p>
+                <p class="h1 d-inline px-5 py-1 bg-light">portfo.io</p>
             </div>
             <div class="justified-content-center">
                 <p class="lead d-inline px-3 py-1 bg-light">Ahmad Saleem Z</p>
