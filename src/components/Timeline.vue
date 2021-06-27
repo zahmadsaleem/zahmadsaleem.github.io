190072affa24b6f9cb08f6a48b1d9018cbc541eb
zahmadsaleem <azsaleem11@gmail.com>
2021-06-27 15:28:27 +0530

    finish pan

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,6 +1,6 @@
 <template>
   <svg height="300px" width="100%" class="mt-2" xmlns="http://www.w3.org/2000/svg" @wheel="setRectWidth"
-       @dblclick="reset">
+       @dblclick="reset" @mousedown="pan">
     <rect v-for="(rect,i) in rectangles" v-bind="rect" :key="i" class="animate-size"></rect>
   </svg>
 </template>
@@ -13,6 +13,7 @@ export default {
       width_: 1,
       width: 1,
       strength: 0.1,
+      panStrength: 0.3,
       rectangles_: [],
       rectangles: []
     }
@@ -39,11 +40,11 @@ export default {
       e.preventDefault()
       const direction = e.wheelDelta / Math.abs(e.wheelDelta)
       this.width = this.width * (1 + this.strength * direction)
-      this.transformRectangles(e.offsetX,direction);
+      this.zoomRectangles(e.offsetX, direction);
     },
-    transform({x, y, width, height}, mouseX, direction) {
+    zoom({x, y, width, height}, mouseX, direction) {
       const ratio = (mouseX - x) / (x + width - mouseX);
-      width = width * (1 + this.strength* direction)
+      width = width * (1 + this.strength * direction)
       return {
         x: mouseX - (ratio * width / (1 + ratio)),
         y,
@@ -51,12 +52,40 @@ export default {
         height
       }
     },
-    transformRectangles(mouseX, direction) {
-      this.rectangles = this.rectangles.map((rect) => this.transform(rect, mouseX, direction))
+    move({x, y, width, height}, delta) {
+      return {
+        x: x + delta,
+        y,
+        width,
+        height
+      }
+    },
+    zoomRectangles(mouseX, direction) {
+      this.rectangles = this.rectangles.map((rect) => this.zoom(rect, mouseX, direction))
     },
     reset() {
       this.rectangles = this.rectangles_
       this.width = this.width_
+    },
+    pan(e) {
+      // starting x
+      let currentX = e.x;
+      const mover = (e_) => {
+        const delta = (e_.x - currentX) * this.panStrength
+        // console.log(delta, e_.button,e);
+        const isButtonPressed = e_.buttons === 1
+        if (!isButtonPressed) {
+          // console.log("removing listener")
+          document.removeEventListener("pointermove", mover)
+          return
+        }
+        // console.log("panning")
+        currentX = currentX + delta;
+        this.rectangles = this.rectangles.map((rect) => this.move(rect, delta))
+      }
+
+      // console.log("adding listener")
+      document.addEventListener("pointermove", mover)
     }
   },
 }
